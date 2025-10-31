ca87bdad45a2f2e4ef185da7b874564d3a0b4738
lichenyi938-dot <lichenyi938@gmail.com>
2025-11-01 01:03:08 +0800

    Update gui_cdPlayer.c

--- original_ok/ESP32_CD_Player-main/main/gui_cdPlayer.c
+++ original_ok/ESP32_CD_Player-main/main/gui_cdPlayer.c
@@ -1,10 +1,13 @@
 // ==== 强制 LVGL 使用项目里的 lv_conf.h，并启用兼容层 ====
+// 如果你的工程已在 CMakeLists 里设置了 LV_CONF_INCLUDE_SIMPLE/LV_CONF_PATH，
+// 这两行即使重复也不会出问题，是安全的双保险。
 #ifndef LV_CONF_INCLUDE_SIMPLE
 #define LV_CONF_INCLUDE_SIMPLE 1
 #endif
-#include "lv_conf.h"        // 可保留；即使不写，LVGL 也会按 SIMPLE 规则找
+#include "lv_conf.h"
+
 #include "lvgl.h"
-#include "lvgl_compat.h"    // ← 我们新加的兼容头，一定要有
+#include "lvgl_compat.h"    // 兼容头：提供 LV_ALIGN_*/LV_SYMBOL_* 等在 v8 的等价实现
 // ============================================================
 
 #include <stdio.h>
@@ -14,8 +17,10 @@
 #include "cdPlayer.h"
 #include "gui_cdPlayer.h"
 
+// ---------------- UI 全局对象 ----------------
 #define AREA_STATUS_BAR_HEIGHT 25
 #define AREA_OSCILLOSCOPE_HEIGHT 65
+
 lv_style_t style_preEmphasis;
 
 lv_obj_t *area_topBar;
@@ -46,26 +51,23 @@ lv_chart_series_t *ser_right;
 lv_obj_t *lb_playState;
 lv_obj_t *lb_volume;
 
-void gui_player_init()
+// ---------------- UI 初始化 ----------------
+void gui_player_init(void)
 {
     lv_color_t color_background = lv_color_make(0x18, 0x18, 0x18);
     lv_color_t color_foreground = lv_color_make(0x1f, 0x1f, 0x1f);
 
-    // 界面
     // main screen
     lv_obj_t *screen = lv_scr_act();
     lv_obj_set_style_bg_color(screen, color_background, 0);
 
     /***********************
      * 背景 - 状态栏
-     * Background - Status Bar
-     */
-
+     ***********************/
     int size_barWidth = 240;
     int size_barHeight = AREA_STATUS_BAR_HEIGHT;
 
     // 顶部状态栏
-    // top
     static lv_style_t style_topBar;
     lv_style_init(&style_topBar);
     lv_style_set_radius(&style_topBar, 0);
@@ -83,7 +85,6 @@ void gui_player_init()
     lv_obj_set_scrollbar_mode(area_topBar, LV_SCROLLBAR_MODE_OFF);
 
     // 底部状态栏
-    // bottom
     static lv_style_t style_bottomBar;
     lv_style_init(&style_bottomBar);
     lv_style_set_radius(&style_bottomBar, 0);
@@ -91,7 +92,6 @@ void gui_player_init()
     lv_style_set_bg_color(&style_bottomBar, lv_color_make(0x86, 0x1b, 0x2d));
     lv_style_set_border_width(&style_bottomBar, 1);
     lv_style_set_border_opa(&style_bottomBar, LV_OPA_COVER);
-    // lv_style_set_border_color(&style_bottomBar, lv_color_make(0x2e, 0x2e, 0x2e));
     lv_style_set_border_color(&style_bottomBar, lv_color_make(0x86, 0x1b, 0x2d));
     lv_style_set_pad_all(&style_bottomBar, 0);
 
@@ -103,8 +103,7 @@ void gui_player_init()
 
     /***********************
      * 背景 - 播放器
-     * Background - Player
-     */
+     ***********************/
     int size_playerWidth = 240;
     int size_playerHeight = 240 - 2 * AREA_STATUS_BAR_HEIGHT - AREA_OSCILLOSCOPE_HEIGHT;
 
@@ -125,8 +124,7 @@ void gui_player_init()
 
     /***********************
      * 背景 - 示波器
-     * Background - Oscilloscope
-     */
+     ***********************/
     int size_oscilloscopeWidth = 240 / 2;
     int size_oscilloscopeHeight = AREA_OSCILLOSCOPE_HEIGHT;
 
@@ -144,8 +142,7 @@ void gui_player_init()
 
     /***********************
      * 光驱型号
-     * CD drive model
-     */
+     ***********************/
     static lv_style_t style1;
     lv_style_init(&style1);
     lv_style_set_text_color(&style1, lv_color_white());
@@ -157,16 +154,14 @@ void gui_player_init()
 
     /***********************
      * 光驱状态
-     * CD drive state
-     */
+     ***********************/
     lb_driveState = lv_label_create(area_topBar);
     lv_obj_add_style(lb_driveState, &style1, 0);
     gui_setDriveState("Start up");
 
     /***********************
      * 专辑名
-     * album title
-     */
+     ***********************/
     lb_albumTitle = lv_label_create(area_player);
     lv_obj_set_style_text_color(lb_albumTitle, lv_color_make(0xc5, 0x86, 0xc0), 0);
     lv_obj_set_style_text_font(lb_albumTitle, &lv_font_montserrat_16, 0);
@@ -176,9 +171,8 @@ void gui_player_init()
     gui_setAlbumTitle("Album-Performer longlonglong");
 
     /***********************
-     * 曲名
-     * track title and performer
-     */
+     * 曲名 & 演唱者
+     ***********************/
     lb_trackTitle = lv_label_create(area_player);
     lv_obj_set_style_text_color(lb_trackTitle, lv_color_make(0xff, 0xff, 0xff), 0);
     lv_obj_set_style_text_font(lb_trackTitle, &lv_font_montserrat_26, 0);
@@ -195,12 +189,12 @@ void gui_player_init()
     lv_obj_set_style_text_align(lb_trackPerformer, LV_TEXT_ALIGN_CENTER, 0);
     lv_label_set_long_mode(lb_trackPerformer, LV_LABEL_LONG_DOT);
 
-    gui_setTrackTitle("What title long long long long long", "Who know long long long long long");
+    gui_setTrackTitle("What title long long long long long",
+                      "Who know long long long long long");
 
     /***********************
-     * 预加重
-     * pre-Emphasis indicator
-     */
+     * 预加重指示
+     ***********************/
     lv_style_init(&style_preEmphasis);
     lv_style_set_border_width(&style_preEmphasis, 0);
     lv_style_set_bg_opa(&style_preEmphasis, LV_OPA_COVER);
@@ -214,13 +208,11 @@ void gui_player_init()
 
     lb_preEmphasized = lv_label_create(area_player);
     lv_obj_add_style(lb_preEmphasized, &style_preEmphasis, 0);
-
     gui_setEmphasis(true);
 
     /***********************
      * 播放时间
-     * Play time
-     */
+     ***********************/
     lb_time = lv_label_create(area_player);
     lv_obj_set_style_text_color(lb_time, lv_color_make(0xdc, 0xdc, 0xaa), LV_PART_MAIN);
     lv_obj_set_style_text_font(lb_time, &lv_font_montserrat_12, LV_PART_MAIN);
@@ -237,44 +229,36 @@ void gui_player_init()
 
     /***********************
      * 进度条
-     * progress bar
-     */
+     ***********************/
     bar_playProgress = lv_bar_create(area_player);
     lv_obj_set_size(bar_playProgress, 230, 11);
     lv_obj_align_to(bar_playProgress, area_player, LV_ALIGN_BOTTOM_MID, 0, -5);
-
     gui_setProgress(111, 999);
 
     /***********************
      * 播放状态
-     * play state
-     */
+     ***********************/
     lb_playState = lv_label_create(area_bottomBar);
     lv_obj_add_style(lb_playState, &style1, 0);
-
     gui_setPlayState(LV_SYMBOL_STOP);
 
     /***********************
      * 音量
-     * volume
-     */
+     ***********************/
     lb_volume = lv_label_create(area_bottomBar);
     lv_obj_add_style(lb_volume, &style1, 0);
-
     gui_setVolume(99);
 
     /***********************
      * 曲目数
-     * number of tracks
-     */
+     ***********************/
     lb_trackNumber = lv_label_create(area_bottomBar);
     lv_obj_add_style(lb_trackNumber, &style1, 0);
     gui_setTrackNum(11, 99);
 
     /***********************
      * 电平表
-     * level meter
-     */
+     ***********************/
     int size_meterWidth = 5;
 
     static lv_style_t style_indic;
@@ -291,10 +275,10 @@ void gui_player_init()
     lv_style_set_border_width(&style_bg, 0);
     lv_style_set_pad_all(&style_bg, 0);
 
-    bar_meterLeft = lv_bar_create(area_left);
+    bar_meterLeft  = lv_bar_create(area_left);
     lv_obj_add_style(bar_meterLeft, &style_bg, 0);
     lv_obj_add_style(bar_meterLeft, &style_indic, LV_PART_INDICATOR);
-    lv_bar_set_range(bar_meterLeft, 35, 90); // 32768:1 = 90.3dB
+    lv_bar_set_range(bar_meterLeft, 35, 90);
     lv_obj_set_size(bar_meterLeft, size_meterWidth, AREA_OSCILLOSCOPE_HEIGHT);
     lv_obj_align_to(bar_meterLeft, area_left, LV_ALIGN_RIGHT_MID, 0, 0);
 
@@ -309,12 +293,10 @@ void gui_player_init()
 
     /***********************
      * 示波器
-     * oscilloscope
-     */
-    size_oscilloscopeWidth = 240 / 2 - size_meterWidth - 1;
+     ***********************/
+    size_oscilloscopeWidth  = 240 / 2 - size_meterWidth - 1;
     size_oscilloscopeHeight = AREA_OSCILLOSCOPE_HEIGHT;
 
-    // 背景样式
     static lv_style_t style_oscilloscope;
     lv_style_init(&style_oscilloscope);
     lv_style_set_radius(&style_oscilloscope, 0);
@@ -324,14 +306,11 @@ void gui_player_init()
     lv_style_set_border_width(&style_oscilloscope, 1);
     lv_style_set_line_color(&style_oscilloscope, lv_color_make(0x33, 0x33, 0x33));
 
-    // 折线样式
     static lv_style_t style_oscilloscopeLine;
     lv_style_init(&style_oscilloscopeLine);
     lv_style_set_line_width(&style_oscilloscopeLine, 1);
 
-    // lv_color_t lineColor = lv_color_make(0x00, 0x8c, 0x3a);
     lv_color_t lineColor = lv_color_make(0x43, 0xd9, 0x96);
-
     int32_t maxPoint = 29000;
 
     chart_left = lv_chart_create(area_left);
@@ -341,9 +320,9 @@ void gui_player_init()
     lv_obj_set_size(chart_left, size_oscilloscopeWidth, size_oscilloscopeHeight);
     lv_obj_align_to(chart_left, area_left, LV_ALIGN_LEFT_MID, -1, 0);
     lv_chart_set_div_line_count(chart_left, 3, 6);
-    lv_chart_set_update_mode(chart_left, LV_CHART_UPDATE_MODE_SHIFT);             // 滚动模式
-    lv_chart_set_point_count(chart_left, size_oscilloscopeWidth);                 // 点数
-    lv_chart_set_range(chart_left, LV_CHART_AXIS_PRIMARY_Y, -maxPoint, maxPoint); // 范围
+    lv_chart_set_update_mode(chart_left, LV_CHART_UPDATE_MODE_SHIFT);
+    lv_chart_set_point_count(chart_left, size_oscilloscopeWidth);
+    lv_chart_set_range(chart_left, LV_CHART_AXIS_PRIMARY_Y, -maxPoint, maxPoint);
 
     chart_right = lv_chart_create(area_right);
     lv_obj_add_style(chart_right, &style_oscilloscope, LV_PART_MAIN);
@@ -356,12 +335,13 @@ void gui_player_init()
     lv_chart_set_point_count(chart_right, size_oscilloscopeWidth);
     lv_chart_set_range(chart_right, LV_CHART_AXIS_PRIMARY_Y, -maxPoint, maxPoint);
 
-    ser_left = lv_chart_add_series(chart_left, lineColor, LV_CHART_AXIS_PRIMARY_Y);
+    ser_left  = lv_chart_add_series(chart_left,  lineColor, LV_CHART_AXIS_PRIMARY_Y);
     ser_right = lv_chart_add_series(chart_right, lineColor, LV_CHART_AXIS_PRIMARY_Y);
-    lv_chart_set_all_value(chart_left, ser_left, 0);
+    lv_chart_set_all_value(chart_left,  ser_left,  0);
     lv_chart_set_all_value(chart_right, ser_right, 0);
 }
 
+// ---------------- UI 更新函数 ----------------
 void gui_setDriveModel(const char *str)
 {
     const char *oldStr = lv_label_get_text(lb_driveModel);
@@ -399,22 +379,16 @@ void gui_setTrackTitle(const char *title, const char *performer)
         lv_label_set_text(lb_trackTitle, title);
 
         bool noPerformer = true;
-        for (int i = 0; i < (int)strlen(performer); i++)
-        {
-            if (performer[i] != ' ')
-            {
-                noPerformer = false;
-                break;
+        if (performer) {
+            for (int i = 0; i < (int)strlen(performer); i++) {
+                if (performer[i] != ' ') { noPerformer = false; break; }
             }
         }
 
-        if (noPerformer)
-        {
+        if (noPerformer) {
             lv_obj_set_height(lb_trackTitle, 60);
             lv_obj_align_to(lb_trackTitle, area_player, LV_ALIGN_TOP_MID, 0, 25);
-        }
-        else
-        {
+        } else {
             lv_obj_set_height(lb_trackTitle, 30);
             lv_obj_align_to(lb_trackTitle, area_player, LV_ALIGN_TOP_MID, 0, 33);
         }
@@ -433,16 +407,12 @@ void gui_setEmphasis(bool en)
     if (oldEn == en) return;
     oldEn = en;
 
-    if (en)
-    {
+    if (en) {
         lv_style_set_bg_opa(&style_preEmphasis, LV_OPA_COVER);
         lv_label_set_text(lb_preEmphasized, "EMPH");
-    }
-    else
-    {
+    } else {
         lv_style_set_bg_opa(&style_preEmphasis, LV_OPA_TRANSP);
-        // 避免零长度格式串告警
-        lv_label_set_text_fmt(lb_preEmphasized, "%s", "");
+        lv_label_set_text(lb_preEmphasized, "");
     }
     lv_obj_align_to(lb_preEmphasized, area_player, LV_ALIGN_TOP_RIGHT, -5, 5);
 }
@@ -452,15 +422,13 @@ void gui_setTime(hmsf_t current, hmsf_t total)
     static hmsf_t oldCurrent = {255, 255, 255, 255};
     static hmsf_t oldTotal   = {255, 255, 255, 255};
 
-    if (memcmp(&oldCurrent, &current, sizeof(hmsf_t)) != 0)
-    {
+    if (memcmp(&oldCurrent, &current, sizeof(hmsf_t)) != 0) {
         memcpy(&oldCurrent, &current, sizeof(hmsf_t));
         lv_label_set_text_fmt(lb_time, "%02d:%02d.%02d", current.minute, current.second, current.frame);
         lv_obj_align_to(lb_time, area_player, LV_ALIGN_BOTTOM_LEFT, 5, -20);
     }
 
-    if (memcmp(&oldTotal, &total, sizeof(hmsf_t)) != 0)
-    {
+    if (memcmp(&oldTotal, &total, sizeof(hmsf_t)) != 0) {
         memcpy(&oldTotal, &total, sizeof(hmsf_t));
         lv_label_set_text_fmt(lb_duration, "%02d:%02d.%02d", total.minute, total.second, total.frame);
         lv_obj_align_to(lb_duration, area_player, LV_ALIGN_BOTTOM_RIGHT, -5, -20);
@@ -470,14 +438,13 @@ void gui_setTime(hmsf_t current, hmsf_t total)
 void gui_setProgress(uint32_t current, uint32_t total)
 {
     static uint32_t oldCurrent = 0;
-    static uint32_t oldToal    = 0;
+    static uint32_t oldTotal   = 0;
 
-    if (oldCurrent == current && oldToal == total) return;
+    if (oldCurrent == current && oldTotal == total) return;
     oldCurrent = current;
-    oldToal    = total;
+    oldTotal   = total;
 
-    if (total == 0) // prevent IntegerDivideByZero exception
-        total = 1234;
+    if (total == 0) total = 1234; // 防止除零
 
     lv_bar_set_range(bar_playProgress, 0, total);
     lv_bar_set_value(bar_playProgress, current, LV_ANIM_OFF);
@@ -485,7 +452,7 @@ void gui_setProgress(uint32_t current, uint32_t total)
 
 void gui_setPlayState(const char *str)
 {
-    const char *oldStr = lv_label_get_text(lb_playState);  // 修正变量名 & const
+    const char *oldStr = lv_label_get_text(lb_playState);
     if (strcmp(oldStr, str) == 0) return;
 
     lv_label_set_text(lb_playState, str);
@@ -498,6 +465,7 @@ void gui_setVolume(int vol)
     if (oldVol == vol) return;
 
     oldVol = vol;
+    // 兼容层里把 lv_label_set_text 重定向到 lv_label_set_text_fmt，因此可以直接用格式化
     lv_label_set_text_fmt(lb_volume, LV_SYMBOL_VOLUME_MAX " %02d", vol);
     lv_obj_align_to(lb_volume, area_bottomBar, LV_ALIGN_RIGHT_MID, -5, 0);
 }
@@ -511,13 +479,9 @@ void gui_setTrackNum(int current, int total)
     oldCurrent = current;
     oldTotal   = total;
 
-    if (total == 0)
-    {
-        // 避免零长度格式串告警
-        lv_label_set_text_fmt(lb_trackNumber, "%s", "");
-    }
-    else
-    {
+    if (total == 0) {
+        lv_label_set_text(lb_trackNumber, "");
+    } else {
         lv_label_set_text_fmt(lb_trackNumber, "%d / %d", current, total);
     }
     lv_obj_align_to(lb_trackNumber, area_bottomBar, LV_ALIGN_CENTER, 0, 0);
@@ -528,13 +492,11 @@ void gui_setMeter(int l, int r)
     static int oldL = -1;
     static int oldR = -1;
 
-    if (oldL != l)
-    {
+    if (oldL != l) {
         oldL = l;
         lv_bar_set_value(bar_meterLeft, l, LV_ANIM_OFF);
     }
-    if (oldR != r)
-    {
+    if (oldR != r) {
         oldR = r;
         lv_bar_set_value(bar_meterRight, r, LV_ANIM_OFF);
     }
